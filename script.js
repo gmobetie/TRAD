9e1df9b31f5ab16d2f3296c0f4cc511c7c791a1d
Raphael Boutin <raphael.boutin@etu.u-bordeaux.fr>
2023-11-17 12:12:40 +0100

    add transition

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const { innerHeight } = window;
 
-gsap.from("#zoom-in img", 
-    { scale: 0.1, stager: 0.25, duration: 3, 
-        scrollTrigger: {
-           trigger: "#zoom-in",
-           pin : true, 
-           end: `+=${innerHeight *1.5}`,
-           scrub: 3
-        } });
+gsap.to("#zoom-in img", {
+  scale: 30,
+  stager: 0.25,
+  duration: 3,
+  scrollTrigger: {
+    trigger: "#zoom-in",
+    pin: true,
+    end: `+=${innerHeight * 1.3}`,
+    scrub: 3,
+    onComplete: function () {
+        gsap.to("#zoom-in img", { opacity: 0, duration: 1 });
+        
+        gsap.to(window, { scrollTo: { y: "main:last-of-type" }, duration: 1, snap: 1 });
+    }
+  },
+});
