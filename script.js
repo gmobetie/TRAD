a98c19f10de69e628a7dbf1f1b527c224dcf269e
Raphael Boutin <raphael.boutin@etu.u-bordeaux.fr>
2023-11-24 16:50:38 +0100

    test sounds

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const { innerHeight } = window;
+gsap.registerPlugin(ScrollTrigger);
+var sound = new Howl({
+  src: ['assets/sound/clochette.mp3']
+});
 
 gsap.to("#zoom-in img", {
   scale: 50,
@@ -9,9 +13,14 @@ gsap.to("#zoom-in img", {
     pin: true,
     end: `+=${innerHeight * 1.3}`,
     scrub: 3,
+    onEnter: () => {
+      Howler.ctx.resume().then(() => {
+        sound.play();
+    });
+    },
+
     onComplete: function () {
         gsap.to("#zoom-in ", { opacity: 0, duration: 1 });
-        
         gsap.to(window, { scrollTo: { y: "main:last-of-type" }, duration: 1, snap: 1 });
     }
   },
