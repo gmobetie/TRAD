ae1ab57c425214e1919325c21814d74aabd3bd34
Raphael Boutin <raphael.boutin@etu.u-bordeaux.fr>
2023-12-09 19:51:30 +0100

    add song test

--- script/3D.js
+++ script/3D.js
@@ -1,47 +1,135 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
-const div = document.getElementById('render');
+const div = document.getElementById("render");
 div.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1D1A1B);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(0, 2, 7);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
+if (window.innerWidth > 1024) {
+  camera.position.set(0, 2, 6);
+} else {
+  camera.position.set(0, 2, 10);
+}
 
-const spotLight = new THREE.SpotLight(0xffffff,  3, 100, 0.22, 1);
+const spotLight = new THREE.SpotLight(0xffffff, 3, 100, 0.22, 1);
 spotLight.position.set(0, 25, 10);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
-const loader = new GLTFLoader().setPath('assets/');
+const loader = new GLTFLoader().setPath("assets/");
 let root;
-loader.load('megaphone.glb', (gltf) => {
-   
-    gltf.scene.scale.set(0.1, 0.1, 0.1); 
-    root = gltf.scene;
-    scene.add(root);
-
+loader.load("megaphone.glb", (gltf) => {
+  gltf.scene.scale.set(0.1, 0.1, 0.1);
+  root = gltf.scene;
+  scene.add(root);
 });
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+  
+});
+
+let rotation = 0.002;
+//on scroll rotation of the megaphone
+window.addEventListener("wheel", (event) => {
+  rotation = event.deltaY * 0.0002;
+});
+
+window.addEventListener("click", (event) => {
+  rotation = 0.002;
 });
 
 function animate() {
   requestAnimationFrame(animate);
   if (root) {
-    root.rotation.y += 0.01;
-}
+    root.rotation.y += rotation;
+  }
   renderer.render(scene, camera);
 }
 
 animate();
+
+//audio 
+
+const c = document.getElementById("audioPlay");
+
+let opt ={
+  width: c.offsetWidth,
+  height: c.offsetHeight,
+  midY: c.offsetHeight / 2,
+  points: 80,
+  stretch: 10,
+  sinHeight: 0,
+  speed:-0.1,
+  strokeColor: "black",
+  strokeWidth: 3,
+  power: false,
+
+};
+
+c.width = opt.width*2;
+c.height = opt.height*2;
+c.style.width = opt.width;
+c.style.height = opt.height;
+
+const ctx = c.getContext("2d");
+ctx.scale(2, 2);
+ctx.strokeStyle = opt.strokeColor;
+ctx.lineWidth = opt.strokeWidth;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+
+let time = 0;
+
+const render = () => {
+  window.requestAnimationFrame(render);
+  ctx.clearRect(0,0,opt.width,opt.height);
+  time += 1;
+  ctx.beginPath();
+  let increment = 0;
+  for (let i = 0; i < opt.points; i++) {
+   if(i<opt.points/2){
+    increment += 0.1;
+    }
+    else{
+      increment += -0.1;
+    }
+    const x = (opt.width / opt.points) * i;
+    const y = opt.midY + Math.sin(time * opt.speed + i / opt.stretch) * opt.sinHeight * increment;
+    ctx.lineTo(x, y);
+  }
+  ctx.stroke();
+};
+render();
+
+c.addEventListener("click", () => {
+  opt.power = !opt.power;
+  if (opt.power) {
+   TweenMax.to(opt, 1, {
+    sinHeight: 15,
+    strech: 5,
+    ease: Power2.easeInOut,
+    });
+  }else{
+    TweenMax.to(opt, 1, {
+      sinHeight: 0,
+      strech: 10,
+      ease: Power3.easeOut,
+      });
+  }
+});
--- script/script-jeu.js
+++ script/script-jeu.js
@@ -1,40 +1,46 @@
 const { innerHeight } = window;
-var sound_neon = new Howl({
-  src: ["assets/sound/neon.mp3"],
-  volume: 0.5,
-  loop: true,
-});
+  var sound_neon = new Howl({
+    src: ["assets/sound/neon.mp3"],
+    volume: 0.5,
+    loop: true,
+  });
 
-var sound_clochette = new Howl({
-  src: ["assets/sound/clochette.mp3"],
-  volume: 1,
-  loop: false,
-});
-
-
-window.addEventListener('scroll', function() {
-  console.log("Taux de défilement :", window.scrollY);
-  if (window.scrollY > 100) {
-    sound_neon.stop();
-  }
-  else {
-    sound_neon.play();
-  }
-  if (window.scrollY > 300 && window.scrollY < 400) {
-    sound_clochette.play();
-  }
-});
-
-
-gsap.to("#magasin", {
-  scale: 50,
-  duration: 3,
-  scrollTrigger: {
-    trigger: "#zoom-in",
-    pin: true,
-    end: `+=${innerHeight * 1.3}`,
-    scrub: 2,
-
-  },
+  var sound_clochette = new Howl({
+    src: ["assets/sound/clochette.mp3"],
+    volume: 1,
+    loop: false,
+  });
+  window.addEventListener("scroll", function () {
+    console.log("Taux de défilement :", window.scrollY);
+    if (window.scrollY > 100) {
+      sound_neon.stop();
+    } else {
+      sound_neon.play();
+    }
+    if (window.scrollY > 300 && window.scrollY < 400) {
+      sound_clochette.play();
+    }
+  });
   
+  gsap.to("#magasin", {
+    scale: 50,
+    duration: 3,
+    scrollTrigger: {
+      trigger: "#zoom-in",
+      pin: true,
+      end: `+=${innerHeight * 1.3}`,
+      scrub: 2,
+    },
+  });
+
+
+var startButton = document.getElementById("start-button");
+var divStart = document.getElementById("start-div");
+var divStart2 = document.getElementById("start-div2");
+
+startButton.addEventListener("click", function () {
+  divStart.style.display = "none";
+  divStart2.style.display = "none";
+  sound_neon.play();
 });
+
