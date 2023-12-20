e5585f5ab13dd2e3ce4569f041158f9f149e4b3b
Raphael Boutin <raphael.boutin@etu.u-bordeaux.fr>
2023-12-20 10:44:08 +0100

    implement

--- script/script-jeu.js
+++ script/script-jeu.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var effetGroupe = document.getElementById("effetGroupe");
+  var vonRestorff = document.getElementById("effetVonRestorff");
+  //displaynone
+  vonRestorff.style.display = "none";
+  effetGroupe.style.display = "none";
+
   const { innerHeight } = window;
   var sound_neon = new Howl({
     src: ["assets/sound/neon.mp3"],
@@ -37,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var divStart = document.getElementById("start-div");
   var divStart2 = document.getElementById("start-div2");
   //impossible de scroll
-  document.body.style.overflow = "hidden";
+  //document.body.style.overflow = "hidden";
 
   startButton.addEventListener("click", function () {
     document.body.style.overflow = "auto";
@@ -46,7 +52,6 @@ document.addEventListener("DOMContentLoaded", function () {
     sound_neon.play();
   });
 
-  var vonRestorff = document.getElementById("effetVonRestorff");
   var main = document.getElementById("main");
   vonRestorff.addEventListener("mouseenter", function () {
     document.body.style.cursor = "none";
@@ -158,7 +163,6 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(function () {
           vonRestorff.style.display = "none";
           window.location.href = "index.html";
-
         }, 2000);
       }
     },
@@ -172,4 +176,52 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     return true;
   }
+
+  const ticketPrev = document.querySelector(".tickets");
+  const tickets = document.querySelectorAll(".tickets__ticket");
+
+  let current = 0;
+
+  tickets.forEach((ticket, index) => {
+    gsap.set(ticket, {
+      zIndex: index,
+      scale: 1 + 0.1 * index,
+      yPercent: 0 + 15 * index,
+    });
+  });
+
+  const animation = function (ticketNumber) {
+    tickets.forEach((ticket, index) => {
+      let ticketOrder = (ticketNumber + index ) % tickets.length;
+      if (ticketOrder < 0) {
+        ticketOrder = ticketOrder + tickets.length;
+      }
+      gsap.to(ticket, {
+        zIndex: ticketOrder,
+        scale: 1 + 0.1 * ticketOrder,
+        yPercent: 0 + 15 * ticketOrder,
+      });
+    });
+  };
+
+  ticketPrev.addEventListener("click", function () {
+    const ticketNumber = current % tickets.length;
+    const currentTicket = tickets[ticketNumber];
+    current = current + 1;
+
+    let direction = "100%";
+
+    if (Math.random() > 0.5) {
+      direction = "-100%";
+    }
+
+    const timeline = gsap.timeline();
+    timeline
+      .set(currentTicket, { x: 0, y: 0, duration: 0.5 })
+      //.to(currentTicket, { x: direction, y: -40, rotation: "5deg" })
+      //.to(currentTicket, { x: 0, y: 0, rotation: "0deg" })
+      .call(animation(ticketNumber));
+  });
+  //forbid acces to next div while you dont click on the first ticket
+
 });
